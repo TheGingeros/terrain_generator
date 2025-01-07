433f3e89f04f7e1caecf8feb1f18dd690155e7e6
TheGingeros <frankie.glazier01@gmail.com>
2025-01-07 19:58:15 +0100

    add function for t he bt

--- CCreateTerrain.py
+++ CCreateTerrain.py
@@ -5,5 +5,11 @@ class OBJECT_OT_create_terrain(bpy.types.Operator):
     bl_idname = "object.create_terrain"
     bl_label = ""
     def execute(self, context):
-        print("Terrain Created")
+        self.createTerrain(context)
         return {'FINISHED'}
+    
+    def createTerrain(self, context):
+        userSize = 20
+        userSubdivision = 5
+        bpy.ops.mesh.primitive_plane_add(size=userSize, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1,1,1))
+        print("Terrain Created")
