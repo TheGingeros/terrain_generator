27da51e39e56cbe66711f831ecb065b5f5286ebe
TheGingeros <frankie.glazier01@gmail.com>
2025-01-08 18:08:32 +0100

    add terrain parameters

---
+++ CUserProperties.py
@@ -0,0 +1,9 @@
+class CUserProperties:
+    def __init__(self):
+        self.terrainSize = 2
+        self.numberOfSubdivision = 10
+        self.terrainVariety = 1
+        self.terrainDetail = 5
+        self.terrainRoughness = 0.5
+        self.terrainDistortion = 0
+        self.randomSeed = 50
