9a0c9c9210dcdfbc4b0e65b34a820201dd26af82
TheGingeros <frankie.glazier01@gmail.com>
2025-01-08 18:22:34 +0100

    change user parameters to bpy properties

--- CUserProperties.py
+++ CUserProperties.py
@@ -1,9 +1,10 @@
-class CUserProperties:
-    def __init__(self):
-        self.terrainSize = 2
-        self.numberOfSubdivision = 10
-        self.terrainVariety = 1
-        self.terrainDetail = 5
-        self.terrainRoughness = 0.5
-        self.terrainDistortion = 0
-        self.randomSeed = 50
+import bpy
+
+class CUserProperties(bpy.types.PropertyGroup):
+    terrainSize: bpy.props.FloatProperty(name="Size of terrain", default=2) # type: ignore
+    numberOfSubdivision: bpy.props.FloatProperty(name="Number of cuts for the grid object", default=10) # type: ignore
+    terrainVariety: bpy.props.FloatProperty(name="Terrain Variety", default=1) # type: ignore
+    terrainDetail: bpy.props.FloatProperty(name="Terrain Detail", default=5) # type: ignore
+    terrainRoughness: bpy.props.FloatProperty(name="Terrain Roughness", default=0.5) # type: ignore
+    terrainDistortion: bpy.props.FloatProperty(name="Terrain Distortion", default=0) # type: ignore
+    randomSeed: bpy.props.FloatProperty(name="Random seed for terrain generation", default=50) # type: ignore
