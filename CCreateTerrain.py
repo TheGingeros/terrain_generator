38175a66bb0bf00d414a4b344a5e790e79a6bdfa
TheGingeros <frankie.glazier01@gmail.com>
2025-01-09 17:47:16 +0100

    link scene parameters to terrainObject nt parameters

--- CCreateTerrain.py
+++ CCreateTerrain.py
@@ -5,12 +5,17 @@ class OBJECT_OT_create_terrain(bpy.types.Operator):
     """Creates terrain object based on the parameters"""
     bl_idname = "object.create_terrain"
     bl_label = ""
+    def __init__(self):
+        self.terrainObject = None
     def execute(self, context):
         self.createTerrain(context)
         return {'FINISHED'}
     def createTerrain(self, context):
-        userSize = 10
-        userSubdivision = 100
+        userSize = context.scene.terrainSize
+        userSubdivision = context.scene.numberOfSubdivision
+        # userSize = 2
+        # userSubdivision = 50
+        print(context.scene.terrainSize)
         bpy.ops.mesh.primitive_grid_add(
             x_subdivisions=userSubdivision, 
             y_subdivisions=userSubdivision, 
@@ -24,13 +29,14 @@ class OBJECT_OT_create_terrain(bpy.types.Operator):
         selectedObjs = bpy.context.selected_objects
         terrainObject = selectedObjs[0]
         terrainObject.name="Terrain"
-        self.createTerrainMaterial(context, terrainObject)
+        self.terrainObject = terrainObject
+        self.createTerrainMaterial(context)
 
         print("Terrain Created")
         context.scene.terrainGenerated = True
 
-    def createTerrainMaterial(self, context, grid):
-        geo_nodes = grid.modifiers.new(name="TerrainGenerator", type='NODES')
+    def createTerrainMaterial(self, context):
+        geo_nodes = self.terrainObject.modifiers.new(name="TerrainGenerator", type='NODES')
         node_group = bpy.data.node_groups.new(name="TerrainGeneratorGroup", type='GeometryNodeTree')
         
         geo_nodes.node_group = node_group
@@ -88,32 +94,76 @@ class OBJECT_OT_create_terrain(bpy.types.Operator):
         # Adjust Noise Texture properties
         noise_texture.inputs["Scale"].default_value = 1.0  # Scale of the terrain features
         noise_texture.inputs["Detail"].default_value = 5.0  # Fine detail in the noise
+        noise_texture.inputs["Roughness"].default_value = 0.5
+        noise_texture.inputs["Distortion"].default_value = 0.0
 
         # Apply smooth shading to the object
         bpy.ops.object.shade_smooth()
+    def updateTerrainValues(self, context):
+        if not context.scene.terrainObject:
+            print("Terrain object no longer exists")
+            return
+        for obj in context.selectable_objects:
+            if obj.name == "Terrain":
+                terrainObject = obj
 
-    bpy.types.Scene.terrainSize = bpy.props.FloatProperty(
+        for mod in terrainObject.modifiers:
+            if mod.type == 'NODES':
+                geoMod = mod
+
+        node_group = geoMod.node_group
+        for node in node_group.nodes:
+            if node.type == 'TEX_NOISE':
+                noiseTexture = node
+        
+        noiseTexture.inputs["Scale"].default_value = context.scene.terrainVariety
+        noiseTexture.inputs["Detail"].default_value = context.scene.terrainDetail
+        noiseTexture.inputs["Roughness"].default_value = context.scene.terrainRoughness
+        noiseTexture.inputs["Distortion"].default_value = context.scene.terrainDistortion
+    def getTerrainObject(self, context):
+        sets = []
+        objs = context.selectable_objects
+        for obj in objs:
+            if obj.name == 'Terrain':
+                sets.append((obj.name, obj.name, obj.name))
+        for set in sets:
+            if (bpy.data.objects.find(set[0]))==-1:
+                sets.remove(set)
+        return sets
+    
+    bpy.types.Scene.terrainObject = bpy.props.EnumProperty(
+         name="terrainObject",
+         items=getTerrainObject,
+         update=updateTerrainValues
+     )
+    bpy.types.Scene.terrainSize = bpy.props.IntProperty(
         name="Size of Terrain", 
         default=2,
-        min=0)
-    bpy.types.Scene.numberOfSubdivision = bpy.props.FloatProperty(
+        min=0,
+        update=updateTerrainValues
+        )
+    bpy.types.Scene.numberOfSubdivision = bpy.props.IntProperty(
         name="Number of cuts for the grid object",
-        default=10,
+        default=50,
         min=0)
     bpy.types.Scene.terrainVariety = bpy.props.FloatProperty(
         name="Terrain Variety",
-        default=1)
+        default=1,
+        update=updateTerrainValues)
     bpy.types.Scene.terrainDetail = bpy.props.FloatProperty(
         name="Terrain Detail",
-        default=5)
+        default=5,
+        update=updateTerrainValues)
     bpy.types.Scene.terrainRoughness = bpy.props.FloatProperty(
         name="Terrain Roughness",
-        default=0.5)
+        default=0.5,
+        update=updateTerrainValues)
     bpy.types.Scene.terrainDistortion = bpy.props.FloatProperty(
         name="Terrain Distortion",
-        default=0)
+        default=0,
+        update=updateTerrainValues)
     bpy.types.Scene.randomSeed = bpy.props.FloatProperty(
-        name="Random seed for terrain generation",
+        name="Random seed for Terrain Generation",
         default=50)
     bpy.types.Scene.terrainGenerated = bpy.props.BoolProperty(
         name="Check if terrain was generated",
